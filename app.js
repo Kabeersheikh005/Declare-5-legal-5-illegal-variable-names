2ae85bf2d452bf4f50dfb9460314a6e61417a0dd
Kabeersheikh005 <kabeersheikh008@gmail.com>
2024-03-26 19:20:35 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Declare 5 legal variables names
+
+let $fullName = "muhammad kabeer";
+console.log($fullName);
+let firstName = "muhammad ";
+console.log(firstName);
+let _last_name = " kabeer";
+console.log(_last_name);
+let age = 18;
+console.log(age);
+let batch_name ="batch11" ;
+console.log(batch_name);
+let NAME = "muhammad kabeer";
+console.log(NAME);
+
+// 5 illegal variable names
+
+// let @name = "muhammad kabeer";
+// console.log(@name);
+// let first-name = "muhammad ";
+// console.log(first-name);
+// let 2lastname = " kabeer";
+// console.log(2lastname);
+// let !Age = 18;
+// console.log(!Age);
+// let batch name ="batch11" ;
+// console.log(batch name);
+// let %name = kabeer;
+// console.log(%name={});
+
+
+
+
+
